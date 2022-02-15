c53b349656e9e3c91e48aaa3281c3ceac2a9a84c
Ajay Anand <ajay.arthur@gmail.com>
2022-02-15 14:30:03 -0500

    Completed login

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from splinter import Browser
+import os
+import dotenv
 
 browser = Browser(driver_name="chrome")
 
+browser.visit('https://ucheck.utoronto.ca')
+
+# Login
+dotenv.load_dotenv()
+browser.find_by_name("j_username").fill(os.getenv("USER"))
+browser.find_by_name("j_password").fill(os.getenv("PASS"))
+browser.find_by_name("_eventId_proceed").click()
